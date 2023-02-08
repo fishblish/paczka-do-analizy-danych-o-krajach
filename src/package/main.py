9091ba0420132db03457ca2ceef1b71edb0fb13e
julia <bartczak.julia.6@gmail.com>
2023-02-09 00:14:47 +0100

    path changes

--- src/package/main.py
+++ src/package/main.py
@@ -1,9 +1,6 @@
 import argparse
 import pandas as pd
-import numpy as np
-from src.package import obrobka_danych as od
-
-
+from . import obrobka_danych as od
 
 #sciezka_bazowa='/home/students/mat/j/jb417512/python/'
 sciezka_bazowa='/home/julia/Documents/python/projekt/'
