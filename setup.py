5f3622dc08fb878f07ab163f1466f106365c207c
julia <bartczak.julia.6@gmail.com>
2023-02-08 16:38:20 +0100

    building package structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='paczka_projekt',
+    author='Julia Bartczak',
+    packages=setuptools.find_packages(),
+    install_requires=["pandas", "regex", "argparse"]
+)
