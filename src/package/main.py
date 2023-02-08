24db4d6d6959f440424fd9543a024b97d8adfba9
julia <bartczak.julia.6@gmail.com>
2023-02-09 00:24:27 +0100

    path changes

--- src/package/main.py
+++ src/package/main.py
@@ -1,14 +1,14 @@
 import argparse
 import pandas as pd
-from . import obrobka_danych as od
+from src.package import obrobka_danych as od
 
 #sciezka_bazowa='/home/students/mat/j/jb417512/python/'
 sciezka_bazowa='/home/julia/Documents/python/projekt/'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("sciezka_gdp", type=str, nargs="?", default= sciezka_bazowa + 'API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv', help="Ścieżka do danych o GDP")
-parser.add_argument("sciezka_populacja", type=str, nargs="?", default= sciezka_bazowa + 'API_SP.POP.TOTL_DS2_en_csv_v2_4751604/API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv', help="Ścieżka do danych o populacji")
-parser.add_argument("sciezka_co2", type=str, nargs="?", default= sciezka_bazowa + 'co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv', help="Ścieżka do danych o emisji co2")
+parser.add_argument("--sciezka_gdp", type=str, nargs="?", default= sciezka_bazowa + 'API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv', help="Ścieżka do danych o GDP")
+parser.add_argument("--sciezka_populacja", type=str, nargs="?", default= sciezka_bazowa + 'API_SP.POP.TOTL_DS2_en_csv_v2_4751604/API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv', help="Ścieżka do danych o populacji")
+parser.add_argument("--sciezka_co2", type=str, nargs="?", default= sciezka_bazowa + 'co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv', help="Ścieżka do danych o emisji co2")
 parser.add_argument("--sd", type=int, default=0, help="Data początkowa")
 parser.add_argument("--ed", type=int, default=0, help="Data końcowa")
 args = parser.parse_args()
